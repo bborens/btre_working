f813e3514b82a74b990c891a88e077d17baa6a48
Bryan B <bborens@wgu.edu>
2018-10-15 15:19:11 -0700

    new

--- accounts/views.py
+++ accounts/views.py
@@ -13,13 +13,21 @@ def register(request):
         password2 = request.POST['password2']
     
         if password == password2:
+
             if User.objects.filter(username=username).exists():
                 messages.error(request, 'Username already taken')
                 return redirect('register')
             else:
                 if User.objects.filter(email=email).exists():
-                messages.error(request, 'email already used')
-                return redirect('register')
+                    messages.error(request, 'email already used')
+                    return redirect('register')
+                else: 
+                    user = User.objects.create_user(username=username, password=password, email=email, first_name=first_name, last_name=last_name)
+                    user.save()
+                    messages.success(request, 'You are now registered and can login. ')
+                    return redirect('login')
+          
+
         else:
             messages.error(request, 'Passwords do not match')
             return redirect('register')
@@ -30,13 +38,26 @@ def register(request):
 
 def login(request):
     if request.method == 'POST':
-        #Login User
-        return 
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            messages.success(request, 'You are now logged in')
+            return redirect('dashboard')
+        else:
+            messages.error(request, 'Invalid Credentials')
+            return redirect('login')
     else:
         return render(request, 'accounts/login.html')
 
 def logout(request):
-    return redirect('index')
+    if request.method == 'POST':
+        auth.logout(request)
+        messages.success(request, 'you are now logged out')
+        return redirect('index')
 
 def dashboard(request):
     return render(request, 'accounts/dashboard.html')
